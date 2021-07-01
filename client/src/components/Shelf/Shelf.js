71e173daf55721a4f874c7bbef5e08953abf8ae7
Nate <n8blake@mac.com>
2021-07-01 13:05:15 -0700

    Update number of books on shelf and shelf style

--- client/src/components/Shelf/Shelf.js
+++ client/src/components/Shelf/Shelf.js
@@ -4,11 +4,11 @@ import './Shelf.scss';
 
 function Shelf(props) {
     //const [books, setBooks] = useState([]);
-
+    // <hr className="shelf-base" />
     return(
         <div className="shelf">
             <div className="books-on-shelf-container">{props.children}</div>
-            <hr className="shelf-base" />
+            
         </div>
         
     );
