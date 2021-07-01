f3b5dbea98163e65c63507c029aaa5597f716304
Nate <n8blake@mac.com>
2021-06-30 22:56:23 -0700

    Comment out console log in API

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,7 +5,7 @@ export default {
     // Get all employees
     search: function(queryString){
         const searchURL = 'https://www.googleapis.com/books/v1/volumes?q=' + queryString;
-        console.log(`Getting ${queryString}`)
+        //console.log(`Getting ${queryString}`)
         return axios.get(searchURL);
     },
     // Get books from our own api
