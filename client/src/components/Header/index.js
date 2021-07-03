eb67851931afd1597706516a87ad015901427f95
Nate <n8blake@mac.com>
2021-07-02 17:18:08 -0700

    Scroll sideways across the shelf

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { useLocation, Link } from 'react-router-dom'
 import './style.scss';
 
 function Header() {
+	const location = useLocation();
+
 	return(
 		<div className="jumbotron">
-			<h1 className="display">The Library</h1>
-			<p>because reading is fundamental</p>
+			<h1 className="display hero-text">The Library</h1>
+			<p className="subtitle hero-subtitle">because reading is fundamental</p>
+			<nav className="pageLinkBtn">
+			{((location.pathname.toLowerCase() ===  '/search') ||  location.pathname.toLowerCase() ===  '/' ) ? (
+				<Link to="/favorites" style={{ textDecoration: 'none' }}>
+					<i class="bi bi-heart"></i>
+					<div class="link-text">favorites</div>
+				</Link>
+			) : (
+				<Link to="/search" style={{ textDecoration: 'none' }}>
+					<i class="bi bi-search"></i>
+					<div class="link-text">search</div>
+				</Link>
+			)}
+			</nav>
 		</div>
 	);
 }
